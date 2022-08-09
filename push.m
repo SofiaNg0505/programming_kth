db45defd38db1c0d943d5d1352058e36e121ef0b
SofiaNg0505 <90323764+SofiaNg0505@users.noreply.github.com>
2022-08-09 16:08:50 +0200

    Add files via upload

---
+++ push.m
@@ -0,0 +1,6 @@
+function stack = push(element, stack);
+% Add an element on the top of a stack, and return the new stack. stack 
+% is a cell array of Matlab strings and element is a Matlab string.
+% YOUR NAME/NAMES, MONTH YEAR
+stack = [stack,element];
+end
