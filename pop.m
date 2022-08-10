62e532ac4fa2d4fb61b1e87c68c393e5f895a0c5
SofiaNg0505 <90323764+SofiaNg0505@users.noreply.github.com>
2022-08-10 12:23:27 +0200

    Delete pop.m

--- pop.m
+++
@@ -1,9 +0,0 @@
-function [element, stack] = pop(stack);
-
-% Remove the top element from a stack, and return this element and the new
-% stack. stack is a cell array of Matlab strings and element is a Matlab 
-% string.
-% YOUR NAME/NAMES, MONTH YEAR
-element = stack(1,end);
-stack = stack(1,1:end-1);
-end
